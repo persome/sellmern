2c6934b9bca423f376bc11bf12a8d6254fb9148c
Mark <muchirimark2@gmail.com>
2022-12-16 22:45:15 +0300

    - Ensured only the logged in user can do CRUD functions for their products only. * Only authenticated user can do the CRUD for products On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backend/controllers/productController.js
    	modified:   backend/routes/productRoutes.js
    	modified:   backend/routes/userRoutes.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productsModel')
+const User = require('../models/userModels')
 
 // @desc Get products
 // @route GET /api/store
 // @ access Private
 // when we use mongoose to interact with the database we get a promise, so we use async await. we need to install an npm package called express-async-handler
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
+    const products = await Product.find({ user: req.user.id })
 
     res.status(200).json(products)
 })
@@ -31,7 +32,8 @@ const setProducts = asyncHandler(async (req, res) => {
         size: req.body.size,
         color: req.body.color,
         prize: req.body.prize,
-        quantity: req.body.quantity
+        quantity: req.body.quantity,
+        user: req.user.id,
     })
 
     res.status(200).json(product)
@@ -47,6 +49,20 @@ const updateProduct = asyncHandler(async (req, res) => {
     if (!product) {
         res.status(400)
         throw new Error('Product not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //make sure the logged in user matches the product user
+    if (product.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -66,6 +82,20 @@ const deleteProduct = asyncHandler(async (req, res) => {
     if (!product) {
         res.status(400)
         throw new Error('Product not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //make sure the logged in user matches the product user
+    if (product.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await product.remove()
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { getProducts, setProducts, updateProduct, deleteProduct } = require('../controllers/productController')
 
-router.route('/').get(getProducts).post(setProducts)
-router.route('/:id').put(updateProduct).delete(deleteProduct)
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getProducts).post(protect, setProducts)
+router.route('/:id').put(protect, updateProduct).delete(protect, deleteProduct)
 
 
 module.exports = router
